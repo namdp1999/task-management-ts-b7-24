e1d28c95bcfe7d184fa52bb949d8adfcd2e03f73
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-11-08 19:36:40 +0700

    2. Tìm kiếm

--- controllers/client/task.controller.ts
+++ controllers/client/task.controller.ts
@@ -32,6 +32,13 @@ export const index = async (req: Request, res: Response) => {
   const skip = (page - 1) * limitItems;
   // Hết Phân trang
 
+  // Tìm kiếm
+  if(req.query.keyword) {
+    const regex = new RegExp(`${req.query.keyword}`, "i");
+    find["title"] = regex;
+  }
+  // Hết Tìm kiếm
+
   const tasks = await Task
     .find(find)
     .limit(limitItems)
