ba4a95566b949eaa7d50f8cea0f473cccdead333
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-11-08 19:33:55 +0700

    1. Phân trang

--- controllers/client/task.controller.ts
+++ controllers/client/task.controller.ts
@@ -17,8 +17,25 @@ export const index = async (req: Request, res: Response) => {
   }
   // End Sort
 
+  // Phân trang
+  let limitItems = 4;
+  let page = 1;
+
+  if(req.query.page) {
+    page = parseInt(`${req.query.page}`);
+  }
+
+  if(req.query.limit) {
+    limitItems = parseInt(`${req.query.limit}`);
+  }
+
+  const skip = (page - 1) * limitItems;
+  // Hết Phân trang
+
   const tasks = await Task
     .find(find)
+    .limit(limitItems)
+    .skip(skip)
     .sort(sort);
 
   res.json(tasks);
